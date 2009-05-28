cd9bbe8ebbe2d9f9a1f720f8ebf352943a651ac2
milaor <milaor@d72e79e6-393f-11de-a15a-d1801d6fabe8>
2009-05-28 11:28:53 +0000

    changing "notfollow"

--- IsraYelp/main.php
+++ IsraYelp/main.php
@@ -28,14 +28,14 @@
 <div id="navContainer">
 		<ul>
 			<LI class="header" id="welcome" ><A class="tabon" href="./">ברוכים הבאים</A> | </LI>
-			<LI class="header" id="aboutMe"><A id="Zabout_me" href="http://static.px.yelp.com/signup?return_url=%2Fuser_details" rel="nofollow">עלינו</A> | </LI>
-			<LI class="header" id="writeReview"><A id="Zwrite_a_review" href="http://static.px.yelp.com/signup?return_url=%2Fwriteareview" rel="nofollow">כתוב ביקורת</A> | </LI>
-			<LI class="header" id="findReview"><A href="http://static.px.yelp.com/find" rel="nofollow">מצא ביקורת</A> | </LI>
-			<LI class="header" id="invite"><A id="Zinvite_friends" href="http://static.px.yelp.com/signup?return_url=%2Finvite_friends" rel="nofollow">הזמן חברים</A></LI>
+			<LI class="header" id="aboutMe"><A rel="nofollow" id="Zabout_me" href="http://static.px.yelp.com/signup?return_url=%2Fuser_details" >עלינו</A> | </LI>
+			<LI class="header" id="writeReview"><A rel="nofollow" id="Zwrite_a_review" href="http://static.px.yelp.com/signup?return_url=%2Fwriteareview" >כתוב ביקורת</A> | </LI>
+			<LI class="header" id="findReview"><A rel="nofollow" href="http://static.px.yelp.com/find" >מצא ביקורת</A> | </LI>
+			<LI class="header" id="invite"><A rel="nofollow" id="Zinvite_friends" href="http://static.px.yelp.com/signup?return_url=%2Finvite_friends" >הזמן חברים</A></LI>
 			
-			<LI class="header_login"><A href=<?php if (session_is_registered('username')) {echo "login.php?logout=1";} else{echo "login.html";}?> rel="nofollow"> <?php if (session_is_registered('username')) {echo "התנתק";} else {echo "כנס";}?></A></LI>
-			<LI class="header_login"><A href=<?php if (session_is_registered('username')) {echo "profile.php";} else{echo "signup.html?profile=1";}?> rel="nofollow">החשבון שלי </A> | </LI>
-			<LI class="header_login"><A href="member_search.php" rel="nofollow"> חיפוש משתמש </A> | </LI>		
+			<LI class="header_login"><A rel="nofollow" href=<?php if (session_is_registered('username')) {echo "login.php?logout=1";} else{echo "login.html";}?> > <?php if (session_is_registered('username')) {echo "התנתק";} else {echo "כנס";}?></A></LI>
+			<LI class="header_login"><A rel="nofollow" href=<?php if (session_is_registered('username')) {echo "profile.php";} else{echo "signup.html?profile=1";}?> >החשבון שלי </A> | </LI>
+			<LI class="header_login"><A rel="nofollow" href="member_search.php" > חיפוש משתמש </A> | </LI>		
 		</ul>
 </div>
 	
@@ -61,7 +61,7 @@
 				
 			</p>
 			<p>
-				<label for="find_loc_ext">ליד <em>כתובת, עיר</em></label>
+				<label for="find_loc_ext">מיקום <em>כתובת, עיר</em></label>
 				<input type="text" maxlength="64" name="find_loc" id="find_loc_ext" tabindex="2" value="חיים לבנון">
 				<input type="hidden" name="ns" value="1">
 				<input type="hidden" name="rpp" value="10">
