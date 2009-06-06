1fbf6e1d5f81d5774bfcb9659339741d4e836bd2
milaor <milaor@d72e79e6-393f-11de-a15a-d1801d6fabe8>
2009-06-06 15:29:17 +0000

    all changes i've made

--- IsraYelp/tel-aviv/restaurants/lukas.php
+++ IsraYelp/tel-aviv/restaurants/lukas.php
@@ -51,25 +51,22 @@
 									<h1 class="fn org">לוקאס</h1>
 									<div id="bizRating">
 										<div class="rating"><img class="stars_4 rating average" width="83" height="325" title="4 star rating" alt="4 star rating" src="http://static.px.yelp.com/static/20090527a/i/new/ico/stars/stars_map.png"/></div> 
-										<em>based on <span class="count">14</span> reviews</em>
-										<div id="rating_graph" class="pseudoLink">Rating Details &raquo;</div>
+										<em>מבוסס על <span class="count">14</span> ביקורות</em>
 									</div>
 								</div>
 								<div id="bizInfoContent">
-									<p id="bizCategories">Categories:
-									<span id="cat_display">				<a  href="/c/sunnyvale-ca/indpak">Indian</a>, 		<a  href="/c/sunnyvale-ca/afghani">Afghan</a>, 		<a  href="/c/sunnyvale-ca/pakistani">Pakistani</a> </span>
-									&nbsp;[<a href="/biz_attribute?biz_id=54anJf73lEHBItVRPgRgrA" id="edit_cat_link" onclick="new Yelp.categoryPickerPopup(this, '54anJf73lEHBItVRPgRgrA'); return false;" class="smallest" rel="nofollow">Edit</a>]
+									<p id="bizCategories">קטגוריה:
+									<span id="cat_display">				<a  href="/c/sunnyvale-ca/indpak">Take Away</a>, 		<a  href="/c/sunnyvale-ca/afghani">אירועים</a>, 		<a  href="/c/sunnyvale-ca/pakistani">ביסטרו</a> </span>
 									</p>
 
 									<address class="adr">
-								
-									<span class="street-address">1208 E Arques Ave<br />Ste 103</span><br /><span class="locality">Sunnyvale</span>, <span class="region">CA</span> <span class="postal-code">94085</span><br />
+										<span class="street-address">מזא"ה 5, תל אביב </span>
 									</address>
 	
-									<span id="bizPhone" class="tel">(408) 733-6345</span>
+									<span id="bizPhone" class="tel">03-5252565, 03-5686820</span>
 	
 									<div id="bizUrl">
-										<a href="http://www.yelp.com/redir?url=http%3A%2F%2Fwww.ichaatcafe.com&amp;src_bizid=54anJf73lEHBItVRPgRgrA" target="_blank" rel="nofollow" class="url">www.ichaatcafe.com</a>
+										<a href="http://www.yelp.com/redir?url=http%3A%2F%2Fwww.ichaatcafe.com&amp;src_bizid=54anJf73lEHBItVRPgRgrA" target="_blank" rel="nofollow" class="url"></a>
 									</div>
 
 								</div>
@@ -77,54 +74,45 @@
 							
 							<div id="bizPhotos">
 								<div class="clearStyles bizPhotoBox">
-										<a  href="/biz_photos/54anJf73lEHBItVRPgRgrA?select=gPtUeg7qvd7TkhUi5RQ6Og"><img src="http://static.px.yelp.com/bphoto/gPtUeg7qvd7TkhUi5RQ6Og/m" style="" alt="iChaat Cafe, Sunnyvale, CA"></a>
+										<a  href="/biz_photos/54anJf73lEHBItVRPgRgrA?select=gPtUeg7qvd7TkhUi5RQ6Og"><img src="./image/lucas.jpg" width=70 height= 100 style="" alt="iChaat Cafe, Sunnyvale, CA"></a>
 								</div>
-								<span style="font-size: 10px;"><a rel="nofollow" href="/biz_photos/54anJf73lEHBItVRPgRgrA?action_user_photos=1">Add Photos</a></span>
+								<span style="font-size: 10px;"><a rel="nofollow" href="../../uploadpic.php">הוסף תמונה</a></span>
 							</div>
 						</div>
 						
 						<div id="bizAdditionalInfo" class="clearfix">
 							<ul>
-								<li><strong>Price Range:</strong> <span class="pricerange"> 
-								<a id="price_tip" title="Price range is the approximate cost per person for a meal including one drink, tax, and tip.&lt;br/&gt;&lt;br/&gt;We&#39;re going for averages here, folks.&lt;br/&gt;&lt;br/&gt;&amp;#36; = Cheap, Under $10&lt;br/&gt;&amp;#36;&amp;#36; = Moderate, $11 - $30&lt;br/&gt;&amp;#36;&amp;#36;&amp;#36; = Spendy, $31 - $60&lt;br/&gt;&amp;#36;&amp;#36;&amp;#36;&amp;#36; = Splurge, Above $60">$</a>
-								</span></li>
-								<li><strong>Accepts Credit Cards:</strong> Yes</li>
-								<li><strong>Parking:</strong> Private Lot</li>
-								<li><strong>Attire:</strong> Casual</li>
-								<li><strong>Good for Groups:</strong> Yes</li>
-								<li><strong>Good for Kids:</strong> Yes</li>
-								<li><strong>Delivery:</strong> Yes</li>
-								<li><strong>Take-out:</strong> Yes</li>
-								<li><strong>Waiter Service:</strong> Yes</li>
-								<li><strong>Outdoor Seating:</strong> Yes</li>
-								<li><strong>Good for:</strong> Dinner</li>
+								<li><strong>מחיר לסועד:</strong> בהתאם לתפריט</li>
+								<li><strong>משלוחים:</strong> כן</li>
+								<li><strong>כשר:</strong> לא</li>
+								<li><strong>גישה לנכים:</strong> כן</li>
+								<li><strong>ידידותי לילדים:</strong> כן</li>
+								<li><strong>אירועים:</strong> כן</li>
+								<li><strong>רומנטי:</strong> כן</li>
+								<li><strong>חניה:</strong> כן</li>
+								<li><strong>אזור עישון:</strong> כן</li>
+								<li><strong>ישיבה בחוץ:</strong> כן</li>
+											
 							</ul>
 						</div>
 					</div>
 				</div>
 				
 				<div id="bizActions" class="clearfix">
-					<a rel="nofollow" href="#" onclick="printSwitch(window.location.href);return false;" id="bizPrintLink">Print version</a>
-					<a class="send-to-friend" rel="nofollow" href="/biz_share?bizid=54anJf73lEHBItVRPgRgrA&amp;return_url=%2Fbiz%2Fichaat-cafe-sunnyvale-3" id="bizShare">Send to Friend</a>
-					<a class="bookmark" rel="nofollow" title="Something catch your eye?<br/>Bookmark this business to build your own list of fun ideas to check out later!" class="bookmark" id="bizBookmark" href="/signup">Bookmark</a>
-					<a class="send-to-phone" rel="nofollow" id="send2phone" href="#" onclick="initBookSignUp(this,'54anJf73lEHBItVRPgRgrA','iChaat Cafe','send2phone');return false">Send to Phone</a>
-					<a class="write review" rel="nofollow" href="/writeareview/biz/54anJf73lEHBItVRPgRgrA" id="bizWriteReview">Write A Review</a>
+					<a class="send-to-friend" rel="nofollow"  href="/biz_share?bizid=54anJf73lEHBItVRPgRgrA&amp;return_url=%2Fbiz%2Fichaat-cafe-sunnyvale-3" id="bizShare"><img src= "../../image/send2friend.png" width=108 height=41></a>
+					<a class="bookmark" rel="nofollow" title="Something catch your eye?<br/>Bookmark this business to build your own list of fun ideas to check out later!" class="bookmark" id="bizBookmark" href="/signup"><img src= "../../image/bookmark.png" width=108 height=41></a>
+					<a class="write review" rel="nofollow" href="/writeareview/biz/54anJf73lEHBItVRPgRgrA" id="bizWriteReview"><img src= "../../image/write.png" width=108 height=41></a>
 				</div>
 			</div>
 			
 			
 			<div id="bizReviews">
 				<div id="bizReviewsHeader" class="clearfix">
-					<form id="review_search_form" method="GET" action="/biz/ichaat-cafe-sunnyvale-3">
-						<p><input type="text" name="q" value="" />
-						<button type="submit" class="form_img_btn alignbtm">Search Reviews</button></p>
-					</form>
-					<h2 id="total_reviews">14 reviews for iChaat Cafe</h2>	
+					<h2 id="total_reviews">14 ביקורות עבור לוקאס:</h2>	
 				</div>
 				<div id="bizReviewsContent">
 					<div id="bizReviewsInner">
-						<DIV id="bizReviewsInner">
-							<H3 id="allReviewHeader" style="DISPLAY: block">All Reviews</H3>
+							<H3 id="allReviewHeader" style="DISPLAY: block">כל הביקורות:</H3>
 							<DIV class="review externalReview clearfix nonfollowingReview " id="review_e9vBvWz7IKmZIuvgHvMjgg">
 								<DIV class="wrap">
 									<DIV class="reviewer">
@@ -146,16 +134,16 @@
 											<IMG class="stars_4" title="4 star rating" height="325" alt="4 star rating" src="http://static.px.yelp.com/static/20090527a/i/new/ico/stars/stars_map.png" width="83" /></DIV>
 											<EM class="smaller">5/14/2009</EM> 
 										</DIV>
-										<P class="review_comment ieSucks">This is a cute little cafe off of Lawrence Expressway. &nbsp;I found it because I was going to the Wells Fargo nearby. <BR /><BR />It is Indian Fast Food. &nbsp;And I know as impossible and improbable as that sounds (as many of us has been traumatized by this genre) it was actually quite good.<BR /><BR />The food was delicious. &nbsp;The prices were affordable. &nbsp;The decor was very modern. &nbsp;It had the atmosphere of a coffee shop you can read a newspaper for hours at -- quiet but not eerie quiet. &nbsp;<BR /><BR />I would recommend this for a quick bite to eat for a chill lunch.</P>
+										<P class="review_comment ieSucks">"יו איזה כיף היה, האוכל נהדר! האנטריקוט שחבר שלי הזמין היה מעולה (ואני לא חובבת אנטריקוט), פירות הים נהדרים (ואל תקשיבו למלצר שמזהיר אתכם מטעם האניס ברוטב... אני שונאת אניס ולא הרגשתי בטעם) והקינוחים סופניים... מומלץ מאוד בעיקר בגלל השירות הסופר מקצועי והאוכל המעולה."
+
+										</P>
 										<UL class="externalReviewActions">
-											<LI class="add-review-comment"><A class="add-owner-comment" href="https://biz.yelp.com/login/add_owner_comment?return_url=%2Fr2r%2F54anJf73lEHBItVRPgRgrA%2Fcomment%2FIZ69bDkna2mmffvoTuyEUA%2Fcompose&biz_id=54anJf73lEHBItVRPgRgrA" rel="nofollow">Add owner comment </A></LI>
-											<LI><A class="bookmark" title="" onclick="initBookSignUp(this,'54anJf73lEHBItVRPgRgrA','iChaat Cafe');return false" href="http://www.yelp.com/signup" rel="nofollow">Bookmark</A> </LI>
-											<LI><A class="sendToFriend" onclick="Yelp.send2Friends.open(this, '54anJf73lEHBItVRPgRgrA', 'e9vBvWz7IKmZIuvgHvMjgg', null);return false" href="http://www.yelp.com/biz_share?bizid=54anJf73lEHBItVRPgRgrA&reviewid=e9vBvWz7IKmZIuvgHvMjgg&return_url=/biz/ichaat-cafe-sunnyvale-3" rel="nofollow">Send to a Friend</A> </LI>
-											<LI><A class="linkToThis" onclick="review_fetcher.fetchReviews('54anJf73lEHBItVRPgRgrA', '', 'e9vBvWz7IKmZIuvgHvMjgg')" href="http://www.yelp.com/biz/ichaat-cafe-sunnyvale-3#hrid:e9vBvWz7IKmZIuvgHvMjgg" rel="nofollow">Link to This Review</A> </LI>
+											<LI><A class="bookmark" title="" onclick="initBookSignUp(this,'54anJf73lEHBItVRPgRgrA','iChaat Cafe');return false" href="http://www.yelp.com/signup" rel="nofollow">סמן כמועדף</A> </LI>
+											<LI><A class="sendToFriend" onclick="Yelp.send2Friends.open(this, '54anJf73lEHBItVRPgRgrA', 'e9vBvWz7IKmZIuvgHvMjgg', null);return false" href="http://www.yelp.com/biz_share?bizid=54anJf73lEHBItVRPgRgrA&reviewid=e9vBvWz7IKmZIuvgHvMjgg&return_url=/biz/ichaat-cafe-sunnyvale-3" rel="nofollow">שלח לחבר</A> </LI>
 										</UL>
 								</div>
 							</div>
-						</div>
+						
 					</div>
 				
 				</div>
@@ -208,3 +196,6 @@
 		<p> זכויות יוצרים </p>
 	</div>
 </div>
+
+</body>
+</html>
