0b0a8085f1bc9a690b6c98a1159623a897b3cb88
yaelamar <yaelamar@d72e79e6-393f-11de-a15a-d1801d6fabe8>
2009-08-24 19:14:44 +0000

    return to rest page if needed.

--- IsraYelp/login.php
+++ IsraYelp/login.php
@@ -20,25 +20,20 @@ function loginUser(){
 	
 	$mysqli = new mysqli('localhost', 'administrator', '', 'test');
 	
-//	/* check connection */
-//	if (mysqli_connect_errno()) {
-//	    printf("Connect failed: %s\n", mysqli_connect_error());
-//	    exit();
-//	}
-	
 	$query = "SELECT * FROM `test`.`users` WHERE email='$email' and password=PASSWORD('$password')";
 	$result = $mysqli->query($query);
 	$count = $result->num_rows;
 	
-//	/* free */
-//    $result->close();
-//    $mysqli->close();
-	
+
 	if($count==1){
 		$user = mysqli_fetch_assoc($result);
 		$_SESSION['username'] = $user['username'];
 		$_SESSION['email'] = $email;
-		header("location:login_success.php");
+		if ($_GET['returnUrl']){
+			header("location:".$_GET['returnUrl']);			
+		} else {
+			header("location:login_success.php");	
+		}
 	} else {
 		echo "דוא'ל או סיסמא לא נכונים";
 	}	
