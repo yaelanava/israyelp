0b7b850392bcee5a3eaf20dab4a4ff35e3491753
yaelamar <yaelamar@d72e79e6-393f-11de-a15a-d1801d6fabe8>
2009-08-24 19:16:54 +0000

    remove "gif" suffix from user pictures

--- IsraYelp/utils/functions.php
+++ IsraYelp/utils/functions.php
@@ -1,7 +1,7 @@
 <?php
 
 function getUserPictureSrc($user_id, $prefix){
-	$src = $prefix."users_pics/".$user_id.".gif";
+	$src = $prefix."users_pics/".$user_id;
 	if (!file_exists($src)) {
 		$src = $prefix."users_pics/blank_user_medium.gif";
 	}
