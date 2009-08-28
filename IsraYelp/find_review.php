484693f39ce34962c261773efb5dec50a6da849d
yaelamar <yaelamar@d72e79e6-393f-11de-a15a-d1801d6fabe8>
2009-08-28 13:46:52 +0000

    fix icon link

--- IsraYelp/find_review.php
+++ IsraYelp/find_review.php
@@ -4,14 +4,14 @@
 
 <html>
 <head>
+	<title>מצא ביקורת | IsraYelp</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=windows-1255">
 	<meta name="description" content="IsraYelp - User Reviews and Recommendations of Top Restaurants, Shopping, Nightlife, Entertainment, Services and More">
 	<meta name="keywords" content="Yelp,recommendation,Israel, review,friend,restaurant,dentist,doctor,salon,spa,shopping,store,share,community,massage,sushi,pizza,nails,ביקורת, מסעדות, בתי קולנוע, מרפאות,מספרות,בתי קפה,חנויות">
 
-	<link rel="shortcut icon" href="../image/favicon.ico" type="image/x-icon">
-	<link rel="icon" href="../image/favicon.ico" type="image/x-icon">
+	<link rel="shortcut icon" href="./image/favicon.ico" type="image/x-icon">
+	<link rel="icon" href="./image/favicon.ico" type="image/x-icon">
 	<link rel="stylesheet" type="text/css" href="./mystyle.css">  	
-	<title>מצא ביקורת | IsraYelp</title>
 
 </head>
 
--- IsraYelp/write_review.php
+++ IsraYelp/write_review.php
@@ -6,15 +6,14 @@ session_start();
 
 <html>
 <head>
+	<title>כתוב ביקורת | IsraYelp</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=windows-1255">
 	<meta name="description" content="IsraYelp - User Reviews and Recommendations of Top Restaurants, Shopping, Nightlife, Entertainment, Services and More">
 	<meta name="keywords" content="Yelp,recommendation,Israel, review,friend,restaurant,dentist,doctor,salon,spa,shopping,store,share,community,massage,sushi,pizza,nails,ביקורת, מסעדות, בתי קולנוע, מרפאות,מספרות,בתי קפה,חנויות">
 
-	<link rel="shortcut icon" href="../image/favicon.ico" type="image/x-icon">
-	<link rel="icon" href="../image/favicon.ico" type="image/x-icon">
+	<link rel="shortcut icon" href="./image/favicon.ico" type="image/x-icon">
+	<link rel="icon" href="./image/favicon.ico" type="image/x-icon">
 	<link rel="stylesheet" type="text/css" href="./mystyle.css">  	
-	<title>כתוב ביקורת | IsraYelp</title>
-
 </head>
 
 <body class= "us" id="IsraYelp_main_body" dir="rtl">
