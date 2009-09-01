3e0c3cfcccc706451d260417627e4e855481f807
yaelamar <yaelamar@d72e79e6-393f-11de-a15a-d1801d6fabe8>
2009-09-01 09:35:56 +0000

    more shopping

--- IsraYelp/utils/insertShopQuery.php
+++ IsraYelp/utils/insertShopQuery.php
@@ -13,25 +13,24 @@ $type = "Shopping Centers";
 $hours = "09:00-22:00";
 
 $query = "INSERT INTO `test`.`shopping` (
-				`id` ,
-				`name` ,
-				`another_name` ,
-				`address` ,
-				`city_id` ,
-				`lat` ,
-				`lon` ,
-				`phone_number` ,
-				`category` ,
-				`grading` ,
-				`num_critiques` ,
-				`hours` ,
-				`invalid_access` ,
-				`parking` ,
-				)	
-			VALUES (
-				NULL, '$name', '$another_name', '$address', '$city_id', NULL, NULL, '$phone', '$type', '0', '0', '$hours', '1', '1'
-			)";
-
+			`id`, 
+			`name`, 
+			`another_name`, 
+			`address`, 
+			`city_id`, 
+			`lat`, 
+			`lon`, 
+			`phone_number`, 
+			`category`, 
+			`grading`, 
+			`num_critiques`, 
+			`hours`, 
+			`invalid_access`, 
+			`parking`
+		) VALUES (
+			NULL, '$name', '$another_name', '$address', '$city_id', NULL, NULL, '$phone', '$type', '0', '0', '$hours', '1', '1'
+		);";
+		
 $result = $mysqli->query($query);
 if ($result) {
 	echo "yes.";
