27376ebbcfffbc46899b5d8989292f0bea5656fa
yaelamar <yaelamar@d72e79e6-393f-11de-a15a-d1801d6fabe8>
2009-09-10 13:40:36 +0000

    sending new place

--- IsraYelp/sending_new_place.php
+++ IsraYelp/sending_new_place.php
@@ -6,19 +6,17 @@ $new_city = $_POST['new_city'];
 $new_comment = $_POST['new_comment'];
 $new_email = $_POST['new_email'];
 	
-$message .= "Place's name: \n";
+$message .= "\nPlace's name: ";
 $message .= $new_name;
-$message .= "\n";
-$message .= "Place's street: \n";
+$message .= "\nPlace's street: ";
 $message .= $new_street;
-$message .= "\n";
-$message .= "Place's city: \n";
+$message .= "\nPlace's city: ";
 $message .= $new_city;
-$message .= "comment: \n";
+$message .= "\ncomment: ";
 $message .= $new_comment;
 
 $to = "israyelp.co.il@gmail.com";
-$subject = "new place: ".$new_name;
+$subject = "new place - $new_name";
 if (mail($to, $subject, $message, "From: $new_email", $new_email)) {
 	header("location:message_sent.php");
 } else {
