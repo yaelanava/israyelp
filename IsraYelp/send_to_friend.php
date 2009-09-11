1026ca180f9460c591f23a320a4a16642266e9cb
yaelamar <yaelamar@d72e79e6-393f-11de-a15a-d1801d6fabe8>
2009-09-11 19:18:57 +0000

    send to friend completed

--- IsraYelp/send_to_friend.php
+++ IsraYelp/send_to_friend.php
@@ -1,10 +1,38 @@
-<?php session_start();?>
+<?php 
+
+session_start();
+
+include './utils/functions.php';
+
+$error_msg = false;
+
+if (isset($_POST['email']) && ('' != $_POST['email'])) {
+	if (check_email($_POST['email'])) {
+		$mysqli = getMysqliConnection();
+	
+		$username = $_SESSION['username'];
+		$email = $_SESSION['email'];
+		
+		$message = $_POST['message'];
+		$message .= "\n\n".(isset($_POST['note']) ? $_POST['note'] : "");
+		
+		$url = $_POST['url'];
+				
+		mail($_POST['email'], "$username ממליצ/ה לך לבקר ב-IsraYelp", $message, "From: $email");
+		//header("Location: ".$url);
+	} else {
+		$error_msg = "דוא'ל לא תקין";
+	}
+}
+
+?>
+
 <html>
 <head>
 	<title>אתר ביקורות הגולשים - IsraYelp </title>
 	<meta http-equiv="Content-Type" content="text/html; charset=windows-1255">
 	<meta name="description" content="IsraYelp - User Reviews and Recommendations of Top Restaurants, Shopping, Nightlife, Entertainment, Services and More">
-	<meta name="keywords" content="Yelp,recommendation,Israel, review,friend,restaurant,dentist,doctor,salon,spa,shopping,store,share,community,massage,sushi,pizza,nails,ביקורת, מסעדות, בתי קולנוע, מרפאות,מספרות,בתי קפה,חנויות">
+	<meta name="keywords" content="israyelp,Yelp,recommendation,Israel, review,friend,restaurant,dentist,doctor,salon,spa,shopping,store,share,community,massage,sushi,pizza,nails,ביקורת, מסעדות, בתי קולנוע, מרפאות,מספרות,בתי קפה,חנויות">
 	
 	<link rel="shortcut icon" href="image/favicon.ico" type="image/x-icon">
 	<link rel="icon" href="image/favicon.ico" type="image/x-icon">
@@ -16,51 +44,50 @@
 <?php echo getNavHTMLCode()?></ul>
 </div>
 
-<script type="text/javascript">
-	function submitToFriends(){
-		$$('#lists_viewport input[type=checkbox]').invoke('disable');
-		document.send_to_a_friend_form.submit();
-	}
-</script>
-
 <div id="bodyContainer">
 	<H1>שלח לחבר</H1>
 		<div>
-		<p> מלא את הפרטים הבאים כדי לשלוח את ההודעה: </p>
-		<br/>
-		<form method="post" action="send_to_friend.php" >
-		<form method="post" action="" name="send_to_a_friend_form" id="send_to_a_friend_form" class="clearfix">
-			<table align="right" cellpadding="5">
-				<tr>
-					<td>
-					<p>אימייל החבר</p>
-					</td>
-					<td>
-						<input type="text" name="emails" class="form200" />
-					</td>
-				</tr>
-				<tr>
-					<td>
-						<p>תוכן ההודעה</p>
-					</td>
-					<td>
-						<textarea id="STF_Note" name="note" rows="5" cols="50" class="form400">מה קורה?</textarea>
-					</td>
-				</tr>
-				<tr>
-					<td>
-						&nbsp;					
-					</td>
-					<td>
-						<p style="text-align:right;margin-top:15px;">
-						<img src="./image/send2friend.png" 
-						onclick="submitToFriends();" 
-						style="cursor:pointer;" alt="Send"></p>
-					
-					</td>
-				</tr>
-			</table>
-		</form>
+			<p> מלא את הפרטים הבאים כדי לשלוח את ההודעה: </p>
+			<br/>
+			<form method="post" action="send_to_friend.php">
+				<table align="right" cellpadding="5">
+					<tr>
+						<td><p>דוא'ל החבר:</p></td>
+						<td><input type="text" name="email" size="50"></td>
+						<?php 
+							if ($error_msg) {
+								echo "<td style=\"color:red;\">$error_msg<td/>";
+							} else if (isset($_POST['email']) && ('' == $_POST['email'])) {
+								echo "<td style=\"color:red;\">* שדה חובה</td>";
+							}
+						?>	
+					</tr>
+					<tr>
+						<td><br/></td>
+					</tr>
+					<tr>
+						<td><p>תוכן ההודעה:</p></td>
+						<?php 
+							$url = $_SERVER['HTTP_REFERER'];
+							$message = "היי!";
+							$message .= " \nאני ממליצ/ה לך להיכנס לאתר IsraYelp ולצפות בדף של המקום הבא:";  
+							$message .= "\n";
+							$message .= $url;
+						?>
+						<td><?php echo $message?></td>
+					</tr>
+					<tr>
+						<td><p>תוכן נוסף:</p></td>
+						<td><textarea id="STF_Note" name="note" rows="5" cols="40" class="form400"></textarea></td>					
+					</tr>
+					<tr>				
+						<td>&nbsp;</td>
+						<td><input type="submit" value="שלח"></td>
+					</tr>
+				</table>
+				<input type="hidden" name="url" value="<?php echo $url?>">
+				<input type="hidden" name="message" value="<?php echo $message?>">
+			</form>
 		</div>
 </div>
 
