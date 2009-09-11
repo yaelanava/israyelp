d5f6d4f478b309cb15601e5c6bf8da214f595606
yaelamar <yaelamar@d72e79e6-393f-11de-a15a-d1801d6fabe8>
2009-09-11 19:42:34 +0000

    fixing bookmark

--- IsraYelp/bookmark.php
+++ IsraYelp/bookmark.php
@@ -5,25 +5,23 @@ include './utils/functions.php';
 
 $mysqli = getMysqliConnection();	
 
-$fav_biz_id = $_GET['fav_biz_id'];
-$fav_biz_type = $_GET['fav_biz_type'];
+$biz_id = $_GET['biz_id'];
+$biz_type = $_GET['biz_type'];
+$biz_url = getBizURL($biz_type, $biz_id);
+
 $user_id = $_SESSION['user_id'];		
-$biz_url = getBizURL($fav_biz_type, $fav_biz_id);
 
-
-$query = "SELECT * FROM `favorites` WHERE user_id='$user_id' 
-		and biz_type='$fav_biz_type' 
-		and biz_id='$fav_biz_id'";
+$query = "SELECT * FROM `favorites` WHERE user_id=$user_id and biz_id=$biz_id and biz_type='$biz_type'";
 $result = $mysqli->query($query);
 $count = $result->num_rows;
 if ($count == 0) {
-	$type_name = mysql_real_escape_string($fav_biz_type);
+	$type_name = mysql_real_escape_string($biz_type);
 	$query = "INSERT INTO .`favorites` (
 				`user_id` ,
 				`biz_id` ,
 				`biz_type` 	
 			)VALUES (
-				'$user_id' , '$fav_biz_id', '$type_name'
+				'$user_id' , '$biz_id', '$biz_type'
 			);";
 	$result = $mysqli->query($query);	
 	if 	($result) {
