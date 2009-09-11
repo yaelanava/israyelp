a096e8b350139b714b3948e2ef05bb370795a240
yaelamar <yaelamar@d72e79e6-393f-11de-a15a-d1801d6fabe8>
2009-09-11 12:27:47 +0000

    updating DB

--- IsraYelp/utils/InsertHotel.php
+++ IsraYelp/utils/InsertHotel.php
@@ -1,37 +1,27 @@
 <?php
-$name = "דן";
-$another_name = "Dan";
-$city_id = 1;
-$address = "הירקון 99 , תל אביב";
-$phone = "03-5202525";
-$lat=32.079496;
-$lon=34.767931;
-$stars=5;
 
-$query = "INSERT INTO `test`.`restaurants` (
-				`id` ,
-				`name` ,
-				`another_name` ,
-				`address` ,
-				`city_id` ,
-				`lat` ,
-				`lon` ,
-				`phone_number` ,
-				`grading` ,
-				`num_critiques` ,
-				`delivery` ,
-				`invalid_access` ,
-				`events` ,
-				`parking` ,
-				`outside` ,
-				`kosher` ,
-				`child_friendly` ,
-				`romantic` ,
-				`smoking` 
-				)	
-			VALUES (
-				NULL, '$name', '$another_name', '$address', '$city_id', NULL, NULL, '$phone', 'events,meat', '0', '0', '1', '1', '1', '1', '0', '0', '1', '1', '0'
-			);";
+include './functions.php';
+
+$mysqli = getMysqliConnection();
+
+//$name = "דן";
+//$another_name = "Dan";
+//$city_id = 1;
+//$address = "הירקון 99 , תל אביב";
+//$phone = "03-5202525";
+//$lat=32.079496;
+//$lon=34.767931;
+//$stars=5;
+
+$query = "INSERT INTO `hotels` (`id`, `name`, `another_name`, `address`, `city_id`, `lat`, `lon`, `phone_number`, `category`, `grading`, `num_reviews`, `link`, `invalid_access`, `parking`, `spa`, `pool`, `kosher`, `kids_club`, `intranet`, `tv`, `mini_bar`, `jim`) VALUES
+(NULL, 'דן', 'Dan', 'הירקון 99 , תל אביב', 1, 32.079496, 34.767931, '03-5202525', 'hotels', 0, 0, NULL, 'כן', 'כן', 'כן', 'כן', 'כן', 'כן', 'כן', 'כן', 'כן', 'לא');
+
+";
 
 $result = $mysqli->query($query);
+if ($result) {
+	echo "nice";
+} else {
+	echo "shit!";
+}
 ?>
