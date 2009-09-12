be0af5715bc8d549cff23d3089da39bc11e37582
yaelamar <yaelamar@d72e79e6-393f-11de-a15a-d1801d6fabe8>
2009-09-12 07:42:03 +0000

    contact_us

--- IsraYelp/contact_us.php
+++ IsraYelp/contact_us.php
@@ -1,8 +1,36 @@
 <?php 
 session_start();
+
 include './utils/functions.php';
+
+$mail_error = false;
+
+if (isset($_POST['email']) && ('' != $_POST['email'])) {
+	if (!check_email($_POST['email'])) {
+		$mail_error = "דוא'ל לא תקין";
+	}
+}
+	
+if (!$mail_error && 
+	isset($_POST['email']) && ('' != $_POST['email']) &&
+	isset($_POST['message']) && ('' != $_POST['message'])) {
+		
+	$mysqli = getMysqliConnection();
+
+	$name = isset($_POST['name']) ? $_POST['name'] : "";
+	
+	$subject = "הודעה ממשתמש $name";
+	$subject = isset($_GET['subject']) ? $subject.": ".$_GET['subject'] : $subject;
+	
+	$message = $_POST['message'];
+				
+	mail("israyelp.co.il@gmail.com", $subject, $message, "From: ".$_POST['email']);
+	header("Location: ./main.php");;
+}
+
 ?>
 
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 	<title>צור קשר | IsraYelp</title>
@@ -16,8 +44,8 @@ include './utils/functions.php';
 </head>
 
 <body dir="rtl">
-<?php echo getHeadHTMLCode()?>
 
+<?php echo getHeadHTMLCode()?>
 
 <div id="bodyContainer">
 	<H1>צור קשר</H1>
@@ -25,53 +53,41 @@ include './utils/functions.php';
 	על מנת לשפר את איכות השירות באתר
 	נשמח לשמוע הערות, הארות והצעות.
 	<br><br>
-	<form action="MAILTO:israyelp@gmail.com" method="post" enctype="text/plain">
-	
-	<table>
-	<tr>
-		<td>
-			<p>שם:</p> 
-		</td>
-		<td>
-			<input type="text" name="name" value="" size="20">
-		</td>
-	</tr>
-	
-	<tr>
-		<td>
-			<p>	כתובת דוא"ל:</p>
-		</td>
-		<td>
-			<input type="text" name="email" value="" size="20">
-		</td>
-	</tr>
-	
-	<tr>
-		<td>
-			<p>נושא:</p>
-		</td>
-		<td>
-			<input type="text" name="subject" value="" size="40">
-		</td>
-	</tr>
-	
-	<tr>
-		<td>
-			<p>תוכן ההודעה:</p>
-		</td>
-		<td>
-			<TEXTAREA name="Comments" rows="5" cols="42"></TEXTAREA>
-		</td>
-	</tr>
-	</table>
+	<form method="post" action="contact_us.php">
+		<table cellspacing="10">
+			<tr>
+				<td><p>שם:</p></td>
+				<td><input name="name" size="50" <?php if (isset($_POST['name'])) echo "value=\"".$_POST['name']."\""?>></td>
+			</tr>
+			<tr>
+				<td><p>	כתובת דוא"ל:</p></td>
+				<td><input name="email" size="50" <?php if (isset($_POST['email'])) echo "value=\"".$_POST['email']."\""?>></td>
+				<?php 
+					if ($mail_error) {
+						echo "<td style=\"color:red;\">$mail_error<td/>";
+					} else if (isset($_POST['email']) && ('' == $_POST['email'])) {
+						echo "<td style=\"color:red;\">* שדה חובה</td>";
+					}
+				?>
+			</tr>
+			<tr>
+				<td><p>נושא:</p></td>
+				<td><input name="subject" size="50" <?php if (isset($_POST['subject'])) echo "value=\"".$_POST['subject']."\""?>></td>
+			</tr>	
+			<tr>
+				<td><p>תוכן ההודעה:</p></td>
+				<td><TEXTAREA name="message" rows="5" cols="40"><?php if (isset($_POST['message'])) echo $_POST['message']?></TEXTAREA></td>
+				<?php if (isset($_POST['message']) && ('' == $_POST['message'])) echo "<td style=\"color:red;\">* שדה חובה</td>"?>					
+			</tr>
+			<tr>
+				<td></td>
+				<td><br><input type="submit" value="שלח"></td>
+			</tr>
+		</table>
 	</form>
-	
-	<br><br>
-	<input type="submit" value="שלח">
-	<input type="reset" value="נקה טופס">
-
-	<br/>
 </div>
+
 <?php echo getFooterHTMLCode()?>
+
 </body>
 </html>
--- IsraYelp/send_message_to_user.php
+++ IsraYelp/send_message_to_user.php
@@ -26,7 +26,8 @@ if (isset($_POST['message']) && ('' != $_POST['message'])) {
 
 ?>
  
- <html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
 <head>
 	<title>שליחת הודעה למשתמש |IsraYelp</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=windows-1255">
