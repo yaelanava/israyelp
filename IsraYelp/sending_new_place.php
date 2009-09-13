0272bb18184c94bb220224dcf64b684b0a96f671
yaelamar <yaelamar@d72e79e6-393f-11de-a15a-d1801d6fabe8>
2009-09-13 20:14:36 +0000

    unneed file

--- IsraYelp/sending_new_place.php
+++ IsraYelp/sending_new_place.php
@@ -18,9 +18,36 @@ $message .= $new_comment;
 $to = "israyelp.co.il@gmail.com";
 $subject = "new place - $new_name";
 if (mail($to, $subject, $message, "From: $new_email", $new_email)) {
-	header("location:message_sent.php");
+	$html = "<p>המייל נשלח בהצלחה.
+				<br><br>
+				<a href=\"./main.php\">חזור לדף הראשי.</a>
+			</p>";
 } else {
-	 header("location:message_failed.php");
+	$html = "<p>שליחת המייל נכשלה.
+				<br><br>
+				<a href=\"./new_place.php\">נסה שנית.</a>
+			</p>";
 }
   	
 ?>
+
+<html>
+<head>
+	<title>הוספת מקום חדש |IsraYelp</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=windows-1255">
+	<meta name="description" content="IsraYelp - User Reviews and Recommendations of Top Restaurants, Shopping, Nightlife, Entertainment, Services and More">
+	<meta name="keywords" content="Yelp,recommendation,Israel, review,friend,restaurant,dentist,doctor,salon,spa,shopping,store,share,community,massage,sushi,pizza,nails,ביקורת, מסעדות, בתי קולנוע, מרפאות,מספרות,בתי קפה,חנויות">
+	
+	<link rel="shortcut icon" href="./image/favicon.ico" type="image/x-icon">
+	<link rel="icon" href="./image/favicon.ico" type="image/x-icon">
+	<link rel="stylesheet" type="text/css" href="./mystyle.css">  	
+</head>
+
+<body dir="rtl">
+<?php echo getHeadHTMLCode()?>
+
+<div id="bodyContainer_Centered">
+	<?php echo $html?>
+</div>
+</body>
+</html>
