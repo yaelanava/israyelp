7c07cc9f6440d902b7bdf11349c2a3f3db3471cb
yaelamar <yaelamar@d72e79e6-393f-11de-a15a-d1801d6fabe8>
2009-09-14 06:50:26 +0000

    better new place

--- IsraYelp/new_place.php
+++ IsraYelp/new_place.php
@@ -1,11 +1,15 @@
 <?php 
-	session_start();
-	include './utils/functions.php';
+
+session_start();
+
+include './utils/functions.php';
+
 ?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-	<title>הוספת מקום חדש - IsraYelp </title>
+	<title>הוספת מקום חדש | IsraYelp </title>
 	<meta http-equiv="Content-Type" content="text/html; charset=windows-1255">
 	<meta name="description" content="IsraYelp - User Reviews and Recommendations of Top Restaurants, Shopping, Nightlife, Entertainment, Services and More">
 	<meta name="keywords" content="Yelp,recommendation,Israel, review,friend,restaurant,dentist,doctor,salon,spa,shopping,store,share,community,massage,sushi,pizza,nails,ביקורת, מסעדות, בתי קולנוע, מרפאות,מספרות,בתי קפה,חנויות">
@@ -15,47 +19,51 @@
 	<link rel="stylesheet" type="text/css" href="./mystyle.css"> 	
 </head>
 
-<body class= "us" id="IsraYelp_main_body" dir="rtl">
+<body dir="rtl">
 
 <?php echo getHeadHTMLCode()?>
 <?php echo getNavHTMLCode()?>
 	
 <div id="bodyContainer">	
-<H1>המקום אינו מופיע במאגר שלנו.</H1>
-<H3>ספר לנו על המקום החדש!</H3>
-<div class="box" id="signupForm">
-	<form method="post" action="sending_new_place.php" >
-		<table cellpadding="5" cellspacing="5" border="0">
-		
-				<tr>
-					<td>שם המקום:</td> 
-					<td><input name="new_name" size="40" value=" <?php echo $_GET['place_name'] ?> "></td>
-				</tr>
-				<tr>
-					<td>רחוב:</td>
-					<td> <input name="new_street" size="40"></td>	
-				</tr>
-				<tr>
-					<td>עיר:</td>
-					<td> <input name="new_city" size="40" value="<?php echo $_GET['place_city'] ?>"></td>	
-				</tr>
-				<tr>
-					<td>ביקורת:</td>
-					<td> <textarea  name="new_comment" rows="5" cols="50" class="form400"></textarea>
-				</tr>
-				<tr>
-					<td>דוא"ל כותב הביקורת:</td> 
-					<td><input name="new_email" size="40" value=""></td>
-				</tr>
-				<tr>
-					<td style="text-align:right;"> <input type="submit" value="שלח"></td>
-					<td></td>
-				</tr>
-				
-		</table>		
-	</form>
+	<H1>המקום אינו מופיע במאגר שלנו.</H1>
+	<H3>ספר לנו על המקום החדש!</H3>
+	<div class="box" id="signupForm">
+		<form method="post" action="sending_new_place.php" >
+			<table cellpadding="5" cellspacing="5" border="0">			
+					<tr>
+						<td>שם המקום:</td> 
+						<td><input name="new_name" size="50" value=" <?php echo $_GET['place_name'] ?> "></td>
+					</tr>
+					<tr>
+						<td>רחוב:</td>
+						<td> <input name="new_street" size="50"></td>	
+					</tr>
+					<tr>
+						<td>עיר:</td>
+						<td> <input name="new_city" size="50" value="<?php echo $_GET['place_city'] ?>"></td>	
+					</tr>
+					<tr>
+						<td>ביקורת:</td>
+						<td> <textarea  name="new_comment" rows="5" cols="40" class="form400"></textarea>
+					</tr>
+					<?php
+						$type = isset($_SESSION['email']) ? "type=\"hidden\"" : "";
+						$value = isset($_SESSION['email']) ? $_SESSION['email'] : "";
+					?>
+					<tr>
+						<?php if (!isset($_SESSION['email'])) echo "<td>דוא\"ל כותב הביקורת:</td>"?> 
+						<td><input <?php echo $type?> name="sender_email" size="50" value="<?php echo $value?>"</td>
+					</tr>
+					<tr>
+						<td style="text-align:right;"> <input type="submit" value="שלח"></td>
+						<td></td>
+					</tr>					
+			</table>		
+		</form>
+	</div>
 </div>
-</div>
-	<?php echo getFooterHTMLCode()?>
+
+<?php echo getFooterHTMLCode()?>
+
 </body>
 </html>
--- IsraYelp/sending_new_place.php
+++ IsraYelp/sending_new_place.php
@@ -4,7 +4,7 @@ $new_name = $_POST['new_name'];
 $new_street = $_POST['new_street'];
 $new_city = $_POST['new_city'];
 $new_comment = $_POST['new_comment'];
-$new_email = $_POST['new_email'];
+$sender_email = $_POST['sender_email'];
 	
 $message .= "\nPlace's name: ";
 $message .= $new_name;
@@ -17,7 +17,7 @@ $message .= $new_comment;
 
 $to = "israyelp.co.il@gmail.com";
 $subject = "new place - $new_name";
-if (mail($to, $subject, $message, "From: $new_email", $new_email)) {
+if (mail($to, $subject, $message, "From: $sender_email")) {
 	$html = "<p>המייל נשלח בהצלחה.
 				<br><br>
 				<a href=\"./main.php\">חזור לדף הראשי.</a>
